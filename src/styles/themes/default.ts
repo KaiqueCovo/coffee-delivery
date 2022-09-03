28c5a3033c81b02fadbba96c13653aeb5123013e
Kaique Covo <kaique.kng@gmail.com>
2022-09-03 17:21:05 -0300

    style: configure styled components

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,25 @@
+export const defaultTheme = {
+  white: '#FFF',
+  background: '#FAFAFA',
+
+  yellow: '#DBAC2C',
+  'yellow-light': '#F1E9C9',
+  'yellow-dark': '#C47F17',
+
+  purple: '#8047F8',
+  'purple-light': '#EBE5F9',
+  'purple-dark': '#4B2995',
+
+  base: {
+    title: '#272221',
+    subtitle: '#403937',
+
+    text: '#574F4D',
+    label: '#8D8686',
+    hover: '#D7D5D5',
+
+    button: '#E6E5E5',
+    input: '#EDEDED',
+    card: '#F3F2F2',
+  },
+};
