6ffecffeb81aa6c5b55c3dc8a14196a0faf24e39
Kaique Covo <kaique.kng@gmail.com>
2022-09-04 13:37:55 -0300

    feature: add header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import { ReactSVG } from 'react-svg';
+
+import logo from '../../assets/logo.svg';
+import { SelectState } from '../SelectState';
+import { ShoppingCart } from '../ShoppingCart';
+import { HeaderContainer } from './styles';
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <ReactSVG src={logo} />
+      <div>
+        <SelectState />
+        <ShoppingCart />
+      </div>
+    </HeaderContainer>
+  );
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  max-width: 1120px;
+
+  margin: 0 auto;
+  padding: 2rem 0;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  > div {
+    display: flex;
+    gap: 0.75rem;
+  }
+`;
